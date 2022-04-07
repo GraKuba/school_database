7170de19b00855a1d680876139bb0f2c2975e09f
Jakub <grabarczyk.kuba@gmail.com>
2022-04-07 15:31:26 +0200

    Creating a list of unique classrooms and taking input from the terminal, then returning adequate data - completed

--- main.py
+++ main.py
@@ -1,40 +1,40 @@
 import sys
-sys.argv = 0
+
 # GLOBAL VARIABLES
 data_storage = []
-
-
+action = sys.argv[1]
+head_teachers = []
+teachers = []
+students = []
+classes_working = []
+classes = []
 # CLASSES
 
 
-class Classroom:
+"""class Klasa:
     def __init__(self, index, head_teacher, student):
         self.index = index
         self.head_teacher = head_teacher
-        self.students = student
-        return
+        self.students = student"""
 
 
-class HeadTeacher:
+class Wychowawca:
     def __init__(self, name, classroom):
         self.name = name
         self.classroom = classroom
-        return
 
 
-class Teacher:
-    def __init__(self, name, classroom, subject):
+class Nauczyciel:
+    def __init__(self, name, subject, classroom):
         self.name = name
         self.classroom = classroom
         self.subject = subject
-        return
 
 
-class Student:
+class Uczen:
     def __init__(self, name, classroom):
         self.name = name
         self.classroom = classroom
-        return
 
 
 # DOWNLOADING DATA FROM IN.TXT
@@ -50,7 +50,6 @@ while True:
                 break
             inputs.append(inp)
         data_storage.append(inputs)
-
     elif description == "nauczyciel":
         full_name = input()
         subject = input()
@@ -70,7 +69,67 @@ while True:
         if False or "stop":
             break
 
+
+# CREATING OBJECTS FOR EACH CLASS
+
 for idx in data_storage:
-    print(idx)
+    if idx[0] == "wychowawca":
+        classes_tpl = idx[2:]
+        classes_working.append(classes_tpl)
+        for person in range(1):
+            head_teachers.append(Wychowawca(idx[1], idx[2:]))
+    elif idx[0] == "nauczyciel":
+        classes_tpl = idx[3:]
+        classes_working.append(classes_tpl)
+        for person in range(1):
+            teachers.append(Nauczyciel(idx[1], idx[2], idx[3:]))
+    elif idx[0] == "uczen":
+        classes_tpl = idx[2:]
+        classes_working.append(classes_tpl)
+        for person in range(1):
+            students.append(Uczen(idx[1], idx[2]))
 
 
+# CREATING A LIST OF UNIQUE CLASSROOMS
+
+for idx in classes_working:
+    for single_idx in idx:
+        if single_idx not in classes:
+            classes.append(single_idx)
+
+
+# TAKING INPUT FROM THE TERMINAL AND RETURNING ADEQUATE INFORMATION
+
+head_teachers_list = []
+student_list = []
+if action in classes:
+    for person in head_teachers:
+        if action in person.classroom:
+            head_teachers_list.append(person.name)
+    for person in students:
+        if action in person.classroom:
+            student_list.append(person.name)
+
+
+print(head_teachers_list)
+print(student_list)
+
+
+"""print("Head Teachers")
+for person in head_teachers:
+    print(f"Full name: {person.name} | Classes: {person.classroom}")
+print()
+for person in teachers:
+    print(f"Full name: {person.name} | Subject: {person.subject} | Classes: {person.classroom}")
+print()
+for person in students:
+    print(f"Full name: {person.name} | Classes: {person.classroom}")"""
+
+"""if action == Klasa.index:
+    ...
+elif action == Wychowawca.name:
+    ...
+elif action == Nauczyciel.name:
+    ...
+elif action == Uczen.name:
+    ..."""
